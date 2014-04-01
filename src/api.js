59c41c100a2bf87800e286188c9c5ebb1e233687
Yang Shu <yangs@punchtab.com>
2014-03-31 21:36:31 -0700

    api update

--- src/api.js
+++ src/api.js
@@ -10,11 +10,18 @@ SteamApi.prototype.ApiRequest = function(hostname, path, params, callback){
 	// params is key:value object
 	var payload = '';
 	for(var key in params){
+		if(params[key] === undefined){
+			return callback({error: 'No steamID detected!'});
+		}
 		payload += ((payload.indexOf('?')==-1 ? '?':'&')+key+'='+params[key]);
 	}
 	var url = hostname + path + payload;
 	request.get(url, function(error, response, body){
-		callback(JSON.parse(body));
+		if(error){
+			callback({error: error});
+		}else if(response.statusCode == 200){
+			callback(JSON.parse(body));
+		}
 	});
 };
 
@@ -30,13 +37,15 @@ SteamApi.prototype.GetPlayerSummaries = function(req, res, steam_id){
 	});
 };
 
-SteamApi.prototype.GetFriendList = function(req, res, steam_id){
+SteamApi.prototype.GetFriendList = function(req, res, steam_id, level){
 	var path = '/ISteamUser/GetFriendList/v0001/';
 	var params = {
 		key: this.deveploer_key,
-		steamids: steam_id,
+		steamid: steam_id,
+		relationship: 'friend',
 		type: 'json'
 	};
+	
 	this.ApiRequest(hostname, path, params, function(json_response){
 		res.json(json_response);
 	});
