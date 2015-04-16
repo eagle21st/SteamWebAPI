46bd6a1150354a34a7aba77a86e4ef11803b08dd
Yang Shu <yangs@punchtab.com>
2015-04-15 18:28:58 -0700

    add api endpoint

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ steam web api
 		api = new steam.Api(<steam_key>);
 ```
 ## OpenID Login:
-	handle login request from user: 
+### handle login request from user: 
 		`auth.authenticate(req,res)`
-	handle callback request from steam: 
+### handle callback request from steam: 
 		```
 		auth.verify(req, res, function(steamId) {
 			// do something with steamid
--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 //  you must have a deployer key to use steam web api
 var rp = require('request-promise'),
 	hostname = 'http://api.steampowered.com',
-	utils = require('../lib/utils.js');
+	utils = require('../lib/utils.js'),
+	_ = require('underscore');
 
 function SteamApi(developerKey){
 	this.developerKey = developerKey;
@@ -19,27 +20,52 @@ function ApiRequest(hostname, path, params){
 	return rp(url);
 }
 
-SteamApi.prototype.GetPlayerSummaries = function(steamIds){
-	// steamIds should be id1,id2,id3...
-	if (utils.isArray(steamIds)) steamIds = utils.flattenArray(steamIds);
-	var path = '/ISteamUser/GetPlayerSummaries/v0002/';
-	var params = {
-		key: this.developerKey,
-		steamids: steamIds,
+function buildParam(key, data, args) {
+	var options = {
+		key: key,
 		type: 'json'
-	};
+	}
+	// extract args from data
+	_.each(args, function(v, k){
+		if (data.hasOwnProperty(k)) {
+			if (v.flatten && utils.isArray(data[k]))
+				options[k] = utils.flattenArray(data[k]);
+			else
+				options[k] = data[k];
+		} else {
+			if (v.required)
+				throw new Error('Missing Params: [' + k, + ']');
+		}
+	});
+	return options;
+}
+
+SteamApi.prototype.GetPlayerSummaries = function(data){
+	// steamIds should be id1,id2,id3...
+	var args = {'steamids': {required: 1, flatten: 1}},
+		path = '/ISteamUser/GetPlayerSummaries/v0002/',
+		params = buildParam(this.developerKey, data, args);
 	return ApiRequest(hostname, path, params);
 };
 
-SteamApi.prototype.GetFriendList = function(steamId){
-	var path = '/ISteamUser/GetFriendList/v0001/';
-	var params = {
-		key: this.developerKey,
-		steamid: steamId,
-		relationship: 'friend',
-		type: 'json'
-	};
-	
+SteamApi.prototype.GetFriendList = function(data){
+	var args = {'steamid': {required: 1}},
+		path = '/ISteamUser/GetFriendList/v0001/',
+		params = buildParam(this.developerKey, data, args);
+	return ApiRequest(hostname, path, params);
+};
+
+SteamApi.prototype.GetRecentlyPlayedGames = function(data) {
+	var args = {'steamid': {required: 1}, 'count': {}},
+		path = '/IPlayerService/GetRecentlyPlayedGames/v0001/',
+		params = buildParam(this.developerKey, data, args);
+	return ApiRequest(hostname, path, params);
+};
+
+SteamApi.prototype.GetOwnedGames = function(data) {
+	var args = {'steamid': {required: 1}, 'include_appinfo': {}, 'include_played_free_games': {}, 'appids_filter': {}},
+		path = '/IPlayerService/GetOwnedGames/v0001/',
+		params = buildParam(this.developerKey, data, args);
 	return ApiRequest(hostname, path, params);
 };
 
